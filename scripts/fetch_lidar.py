f7218f413ceea7a63c05782e42edaee8abd3c5e4
Diye Mark <diyye101@gmail.com>
2022-06-24 08:51:22 +0300

    wip: fetch and load

---
+++ scripts/fetch_lidar.py
@@ -0,0 +1,6 @@
+class FetchLidar:
+    def __init__(self):
+        pass
+
+    def fetch_lidar_data(self):
+        pass
---
+++ scripts/get_metadata.py
@@ -0,0 +1,6 @@
+class GetMetadata:
+    def __init__(self):
+        pass
+
+    def get_metadata(self):
+        pass
