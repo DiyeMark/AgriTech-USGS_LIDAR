195e443e3549466f10837b75c0f62dde75a91bea
Diye Mark <diyye101@gmail.com>
2022-06-27 01:21:48 +0300

    done: fetch and load

---
+++ scripts/lidar.py
@@ -0,0 +1,11 @@
+from shapely.geometry import Polygon
+
+from scripts.fetch_lidar import FetchLidar
+
+
+class Lidar:
+    def __init__(self):
+        self._fetch_lidar = FetchLidar()
+
+    def fetch_lidar(self, polygon: Polygon, regions=[]):
+        return self._fetch_lidar.fetch_lidar_data(polygon, regions)
